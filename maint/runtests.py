69777a6eb019b02ba731cfd83dfa667be7c5cd3a
woorlds <woorlds@gmail.com>
2013-05-27 18:51:52 +0900

    Add test code

---
+++ maint/runtests.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestFunctions(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+  def test_sample(self):
+    self.assetEqual(1, 1)
+
